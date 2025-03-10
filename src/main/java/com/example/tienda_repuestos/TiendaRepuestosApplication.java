409e084e5500a768935fe985861c6a343b3dd006
JoseMontesU <jmontesu@bcds.net>
2025-03-09 19:20:45 -0500

    creacion del proyecto backend

---
+++ src/main/java/com/example/tienda_repuestos/TiendaRepuestosApplication.java
@@ -0,0 +1,13 @@
+package com.example.tienda_repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRepuestosApplication.class, args);
+	}
+
+}
